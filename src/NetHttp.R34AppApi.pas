438ad5b07e89c7b221d5d023cd18c3b7f3384a0f
kisspeace <kisspeace@github.com>
2022-06-20 02:18:22 -0400

    init

---
+++ src/NetHttp.R34AppApi.pas
@@ -0,0 +1,80 @@
+﻿//♡2022 by Kisspeace. https://github.com/kisspeace
+unit NetHttp.R34AppApi;
+
+interface
+uses
+  R34App.Types, Net.HttpClientComponent,
+  Net.HttpClient, XSuperObject, SysUtils;
+
+const
+  R34APP_API_URL = 'https://api.r34.app';
+
+type
+
+  TR34AppClient = class(TObject)
+    private
+      FBooru: string;
+    public
+      WebClient: TNetHttpClient;
+      function GetPosts(ATags: string; APageId: integer = 1;
+        ALimit: integer = 100): TR34AppItems;
+      property Booru: string read FBooru write FBooru;
+      constructor Create;
+      destructor Destroy; override;
+  end;
+
+implementation
+uses unit1;
+
+{ TR34Client }
+
+constructor TR34AppClient.Create;
+begin
+  WebClient := TNetHttpClient.Create(nil);
+  WebClient.SynchronizeEvents := true;
+  WebClient.Asynchronous := false;
+  Booru := 'rule34.xxx';
+end;
+
+destructor TR34AppClient.Destroy;
+begin
+  WebClient.Free;
+  inherited;
+end;
+
+function TR34AppClient.GetPosts(ATags: string; APageId: integer;
+  ALimit: integer): TR34AppItems;
+var
+  X: ISuperArray;
+  url: string;
+  Response: IHTTPResponse;
+  Content: string;
+begin
+  Result := nil;
+  url :=
+    ( R34APP_API_URL + '/booru/' +
+      FBooru + '/posts?' +
+      'baseEndpoint=' + FBooru +
+      '&limit=' + ALimit.ToString +
+      '&pageID=' + APageId.ToString );
+
+  if not ATags.IsEmpty then
+    url := url + '&tags=' + ATags;
+
+  Response := WebClient.Get(url);
+
+  try
+    Content := Response.ContentAsString;
+    X := SA(Content);
+    try
+      if X.Length > 0 then
+        Result := TJson.Parse<TR34AppItems>(X);
+    except
+      Result := nil;
+    end;
+  Except
+    Result := nil;
+  end;
+end;
+
+end.
---
+++ src/R34App.Types.pas
@@ -0,0 +1,54 @@
+﻿//♡2022 by Kisspeace. https://github.com/kisspeace
+unit R34App.Types;
+
+interface
+
+type
+
+  TR34AppFile = record
+    url: string;
+    width: integer;
+    height: integer;
+  end;
+
+  TR34AppItem = record
+    id: int64;
+    score: integer;
+    tags: TArray<string>;
+    high_res_file: TR34AppFile;
+    low_res_file: TR34AppFile;
+    preview_file: TR34AppFile;
+    source: TArray<string>;
+    rating: string;
+    media_type: string;
+    function IsImage: boolean;
+    function TagsCount: integer;
+    function TagsStr: string;
+  end;
+
+  TR34AppItems = TArray<TR34AppItem>;
+
+implementation
+
+{ TR34AppItem }
+
+function TR34AppItem.IsImage: boolean;
+begin
+  Result := (Self.media_type = 'image');
+end;
+
+function TR34AppItem.TagsCount: integer;
+begin
+  Result := Length(Tags);
+end;
+
+function TR34AppItem.TagsStr: string;
+var
+  I: integer;
+begin
+  Result := '';
+  for I := Low(tags) to High(tags) do
+    Result := Result + ' ' + Tags[I];
+end;
+
+end.
